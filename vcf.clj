b1c1f8a9840a527993c40253a66516a1c49900df
Jan Aerts <jan.aerts@gmail.com>
2010-07-01 20:40:32 +0100

    Working towards getting info from INFO field

--- vcf.clj
+++ vcf.clj
@@ -57,10 +57,28 @@
 	[filename]
 	(dataset (column-names filename) (map #(qual-as-float %) (parsed-data-lines filename))))
 
+(defn parse-info
+	[line]
+	(let [fields (split line #";")]
+		(map #(split % #"=") fields)))
+
+;(doseq [l (map parse-info (map :info (:rows a)))]
+;  (println ((apply hash-map (flatten (filter #(= (count %) 2) l ))) "DP")))
+
+
+(defn get-info
+	[tag dataset]
+	(doseq [l (map parse-info (map tag (:rows dataset)))]
+		(println ((apply hash-map (flatten (filter #(= (count %) 2) l))) "AA"))))
 
 ;;;;;;;;;;;;;;;;;;
 
-(use '[clojure.set])
+(def a (read-vcf "../data/sample.vcf"))
+
+
+;;;;;;;;;;;;;;;;;;
+
+;(use '[clojure.set])
 ;(def w (sel (read-vcf "w.vcf") :cols [:chrom :pos :qual]))
 ;(def w_ids (map #(str (:chrom %) "_" (:pos %)) (:rows w)))
 ;(def b (sel (read-vcf "b.vcf") :cols [:chrom :pos :qual]))
@@ -72,24 +90,24 @@
 ;(def d (col-names (conj-cols c c_ids) [:qual1 :pos1 :chrom1 :id]))
 ;(def x (col-names (conj-cols w w_ids) [:chrom2 :pos2 :qual2 :id]))
 ;(def y (dataset [:id :chrom1 :pos1 :qual1 :chrom2 :pos2 :qual2] (data-merge :id (:rows d) (:rows x))))
-(defn data-merge
-  "Merge two data sets on the given key"
-  [merge-key a b]
-  (let [indexed-b (zipmap (map merge-key b) b)]
-    (map #(into % (indexed-b (merge-key %))) (filter #(contains? indexed-b (merge-key %)) a))))
+;(defn data-merge
+;  "Merge two data sets on the given key"
+;  [merge-key a b]
+;  (let [indexed-b (zipmap (map merge-key b) b)]
+;    (map #(into % (indexed-b (merge-key %))) (filter #(contains? indexed-b (merge-key %)) a))))
+;
+;(def v (sel (read-vcf "v.vcf") :cols [:chrom :pos :qual]))
+;(def v_ids (map #(str (:chrom %) "_" (:pos %)) (:rows v)))
+;(def v (col-names (conj-cols v_ids v) [:chrom-pos :chrom :pos :qual]))
+;(def golden (col-names (read-dataset "golden_standard_na12878_filtered_CTR_and_exons.tsv" :delim \tab) [:id-golden :is-present]))
+;(def v-golden ($join [:id-golden :chrom-pos] golden v))
 
-(def v (sel (read-vcf "v.vcf") :cols [:chrom :pos :qual]))
-(def v_ids (map #(str (:chrom %) "_" (:pos %)) (:rows v)))
-(def v (col-names (conj-cols v_ids v) [:chrom-pos :chrom :pos :qual]))
-(def golden (col-names (read-dataset "golden_standard_na12878_filtered_CTR_and_exons.tsv" :delim \tab) [:id-golden :is-present]))
-(def v-golden ($join [:id-golden :chrom-pos] golden v))
-
-(def a (sel (read-vcf "a.vcf") :cols [:chrom :pos :qual]))
-(def a_ids (map #(str (:chrom %) "_" (:pos %)) (:rows a)))
-(count (intersection (set a_ids) (set v_ids)))
-(def a (col-names (conj-cols a a_ids) [:chrom1 :pos1 :qual1 :id]))
-(def v (col-names (conj-cols v v_ids) [:chrom2 :pos2 :qual2 :id]))
-
-(def i (dataset [:id :chrom1 :pos1 :qual1 :chrom2 :pos2 :qual2] (data-merge :id (:rows a) (:rows v))))
-(save i "intersection.tsv")
-(view (scatter-plot :qual1 :qual2 :data i))
+;(def a (sel (read-vcf "a.vcf") :cols [:chrom :pos :qual]))
+;(def a_ids (map #(str (:chrom %) "_" (:pos %)) (:rows a)))
+;(count (intersection (set a_ids) (set v_ids)))
+;(def a (col-names (conj-cols a a_ids) [:chrom1 :pos1 :qual1 :id]))
+;(def v (col-names (conj-cols v v_ids) [:chrom2 :pos2 :qual2 :id]))
+;
+;(def i (dataset [:id :chrom1 :pos1 :qual1 :chrom2 :pos2 :qual2] (data-merge :id (:rows a) (:rows v))))
+;(save i "intersection.tsv")
+;(view (scatter-plot :qual1 :qual2 :data i))
