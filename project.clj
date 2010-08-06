8a7a8d17af41819a74a1ffc9b495916d52f75a77
Jan Aerts <jan.aerts@gmail.com>
2010-08-06 17:48:12 +0100

    Made it possible to distribute bioclojure and have a 'bioclojure' command using :gen-class

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject bioclojure "0.0.1-SNAPSHOT"
   :description "A clojure library for the biological sciences"
   :dependencies [[org.clojure/clojure "1.2.0-beta1"]
-                 [org.clojure/clojure-contrib "1.2.0-beta1"]
-                 [incanter/incanter "1.2.3-SNAPSHOT"]]
+                 [org.clojure/clojure-contrib "1.2.0-beta1"]]
+  :main bioclojure
   :namespaces [bioclojure])
 
--- src/bioclojure.clj
+++ src/bioclojure.clj
@@ -1,3 +1,25 @@
+;; To generate the uberjar:
+;; - lein deps
+;; - lein compile
+;; - lein uberjar
 (ns bioclojure
   (:use [bioclojure.vcf])
+  (:gen-class)
 )
+
+(def help-string
+  "The bioclojure command takes a list of arguments. The first one is always the 
+  command that should be run, for example: 'java -jar bioclojure.jar say-hi'. There
+  might be additional arguments that will act as the input for the command that
+  is selected. Try:
+  * java -jar bioclojure.jar say-hi
+  * java -jar bioclojure.jar help
+  * java -jar bioclojure.jar vcf2tsv ./data/sample.vcf ./data/sample.tsv")
+
+(defn -main
+  [command & args]
+  (case command
+        "help" (println help-string)
+        "vcf2tsv" (vcf2tsv (first args) (second args))
+        "say-hi" (println "Hi there! This actually works...")
+        (println "Unknown command")))
